12084e339a4e4f3015b1ef3abcc862fa6925f96f
afrobug <afrojahmed6@gmail.com>
2023-04-20 19:23:08 +0530

    landing about page added

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ document.getElementById('previous').addEventListener('click', ()=>{
 
 //Spotify authorization 
 
-var redirect_uri = "http://127.0.0.1:5503/index.html"; // change this your value
+var redirect_uri = "http://127.0.0.1:5500/index.html"; // change this your value
 
 
 
@@ -126,7 +126,7 @@ var refresh_token = null;
 var currentPlaylist = "";
 var radioButtons = [];
 
-// This app demostrates how the use the following APIs:
+// how the use the following APIs:
 
 const AUTHORIZE = "https://accounts.spotify.com/authorize";
 const TOKEN = "https://accounts.spotify.com/api/token";
@@ -143,7 +143,8 @@ const CURRENTLYPLAYING = "https://api.spotify.com/v1/me/player/currently-playing
 const SHUFFLE = "https://api.spotify.com/v1/me/player/shuffle";
 
 function onPageLoad() {
-    client_id = localStorage.getItem("client_id");
+    // console.log("checking on page load rendering")
+    client_id = localStorage.getItem("bc767bed85484b1eafed7edddcebb9f2");
     client_secret = localStorage.getItem("client_secret");
     if (window.location.search.length > 0) {
         handleRedirect();
@@ -182,17 +183,20 @@ function getCode() {
 }
 
 function requestAuthorization() {
-    client_id = document.getElementById("clientId").value;
-    client_secret = document.getElementById("clientSecret").value;
+    client_id = "bc767bed85484b1eafed7edddcebb9f2";
+    client_secret = "9fb46c42eb5d43adafd98a882539a07c";
     localStorage.setItem("client_id", client_id);
     localStorage.setItem("client_secret", client_secret); // In a real app you should not expose your client_secret to the user
 
-    let url = AUTHORIZE;
+    // let url = AUTHORIZE;
+    let url = "https://accounts.spotify.com/authorize";
     url += "?client_id=" + client_id;
     url += "&response_type=code";
+    // url += "&redirect_uri=" + encodeURI(redirect_uri);
     url += "&redirect_uri=" + encodeURI(redirect_uri);
     url += "&show_dialog=true";
     url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    console.log("the url is", url)
     window.location.href = url; // Show Spotify's authorization screen
 }
 
